eac501a3a7e3958fad3d01dab65a16e19ce0dbea
invisiblesheep <tuomas.jokioja@gmail.com>
2016-05-06 20:13:02 +0300

    Lotsa changes

--- rest/server.js
+++ rest/server.js
@@ -1,8 +1,10 @@
 
 //Dependencies
+var http = require('http');
 var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var path = require('path');
 
 //MongoDB
 mongoose.connect('mongodb://localhost/rest_test');
@@ -11,11 +13,21 @@ mongoose.connect('mongodb://localhost/rest_test');
 var app = express();
 app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
+app.set('port', process.env.PORT || 3000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
 
 // Routes
 app.use('/api', require('./routes/api'));
+app.get('/', function (req, res) {
+    res.send('<html><body><h1>Flagon</h1></body></html>');
+});
+app.use(function (req, res) {
+    res.render('404', {url:req.url});
+});
 
 
 // Start server
-app.listen(3000);
-console.log('API is running on port 3000');
+http.createServer(app).listen(app.get('port'), function(){
+    console.log('Express server listening on port ' + app.get('port'));
+})
